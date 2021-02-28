70c022a680de206bc549297ce6510eae37abc215
gedetrt <trisna.quebe@hva.nl>
2021-02-28 01:08:39 +0100

    Cleanup the wrong files

--- chmod_shadow/compile.sh
+++
@@ -1,3 +0,0 @@
-
-
-nasm -f elf chmod_shadow.asm && ld -o chmod_shadow chmod_shadow.o -m elf_i386 && strace ./chmod_shadow
--- exit_shellcode/exit_shellcode.asm
+++
@@ -1,8 +0,0 @@
-; Author:	Trisna Quebe
-
-Section .text
-	global _start
-_start:
-	mov ebx,0
-	mov eax,1
-	int 0x80
